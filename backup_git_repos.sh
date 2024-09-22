1392db5477e1de9d3328e989ead54569ed9def1d
SchokiCoder <afschoknecht@tutanota.com>
2024-09-22 23:26:01 +0200

    update repo list for backups

--- backup_git_repos.sh
+++ backup_git_repos.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 URL="https://github.com/SchokiCoder/"
-REPOS="2d 5th_2d 5th_schoki_game 5th_schoki_gui 5th_schoki_misc c2_2d c_50_years_old_foundation genarium gohui experiments hui hawps ms_office_macros onewaypass rshui remote_control scripts smng troll_erp twilights_program python_tutorial"
+
+# empty backticks ` evaluate to nothing... yeah
+REPOS="2d 5th_2d 5th_schoki_game 5th_schoki_gui 5th_schoki_misc "`
+	`"c2_2d c_50_years_old_foundation "`
+	`"experiments flathub gohui hui hawps ms_office_macros "`
+	`"onewaypass rshui remote_control scripts smng sonne "`
+	`"troll_erp twilights_program python_tutorial"
 
 for ITEM in $REPOS; do
 	if [ ! -d "$ITEM" ]; then
